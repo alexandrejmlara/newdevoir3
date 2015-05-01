108627d29b368e55c68aa98de1f84e8929a51927
Rodolpho Pinheiro <rodolphopinheiro@live.com>
2015-05-01 04:46:14 -0400

    atualizaçoes

--- newdevoir3/src/newdevoir3/Trace.java
+++ newdevoir3/src/newdevoir3/Trace.java
@@ -20,37 +20,40 @@ public class Trace {
 			this.longueur = 1;
 	}
 	
-public void allonge(char direction) {
-		
-		int x, y;
-		
-		switch(direction){
-			case 'n': // direction vers le haut
-				x = positionX;
-				y = positionY - 1;
-				this.trace.add(new Point(x, y));
-				this.longueur++;
-				break;
-			case 'w': // direction vers la gauche (west)
-				x = positionX - 1;
-				y = positionY;
-				this.trace.add(new Point(x, y));
-				this.longueur++;
-				break;
-			case 's': // direction vers le bas
-				x = positionX;
-				y = positionY + 1;
-				this.trace.add(new Point(x, y));
-				this.longueur++;
-				break;
-			case'e': // direction vers la droite (east)
-				x = positionX + 1;
-				y = positionY;
-				this.trace.add(new Point(x, y));
-				this.longueur++;
-				break;
-			default:
-				System.out.println("commande invalide");
-		}
+	public void allonger(char direction) {
+			
+			int x, y;
+			
+			switch(direction){
+				case 'n': // direction vers le haut
+					x = positionX;
+					y = positionY - 1;
+					this.trace.add(new Point(x, y));
+					this.longueur++;
+					break;
+				case 'w': // direction vers la gauche (west)
+					x = positionX - 1;
+					y = positionY;
+					this.trace.add(new Point(x, y));
+					this.longueur++;
+					break;
+				case 's': // direction vers le bas
+					x = positionX;
+					y = positionY + 1;
+					this.trace.add(new Point(x, y));
+					this.longueur++;
+					break;
+				case'e': // direction vers la droite (east)
+					x = positionX + 1;
+					y = positionY;
+					this.trace.add(new Point(x, y));
+					this.longueur++;
+					break;
+				default:
+					System.out.println("commande invalide");
+			}
 	}
+	
 }
+
+
